1098f205272d3c1caea7b79156314b05b171f867
paulohfsantos <paulohfsantos1@gmail.com>
2023-08-07 00:52:44 -0300

    chore: dailyui elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 import { themeChange } from "theme-change";
+import { useAuth } from "../hooks/useAuth";
 
 export const Header = () => {
+  const { logout } = useAuth();
+
   const themes = [
     "light",
     "dark",
@@ -32,17 +35,25 @@ export const Header = () => {
   }, []);
 
   return (
-    <div className="p-4">
-      <select
-        data-choose-theme
-        className="select select-bordered w-full max-w-xs"
-      >
-        {themes.map((theme) => (
-          <option key={theme} value={theme}>
-            {theme}
-          </option>
-        ))}
-      </select>
+    <div className="w-full px-4 py-2 flex items-center justify-between">
+      <div className="logo">
+        <h5 className="font-bold">Home</h5>
+      </div>
+      <div className="flex">
+        <select
+          data-choose-theme
+          className="select select-bordered w-full max-w-xs select-sm"
+        >
+          {themes.map((theme) => (
+            <option key={theme} value={theme}>
+              {theme}
+            </option>
+          ))}
+        </select>
+        <button className="btn btn-sm" onClick={logout}>
+          Logout
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { useAuth } from "../hooks/useAuth";
 import { Header } from "../components/Header";
 
 export const Home: React.FC = () => {
-  const { logout } = useAuth();
-
   return (
     <div className="flex flex-col min-h-screen items-center justify-between">
       <Header />
-      <h1 className="text-4xl font-bold">Home</h1>
-      <button className="btn" onClick={logout}>
-        Logout
-      </button>
+
+      <div className="container mx-auto px-4">
+      <div className="card glass w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      </div>
+
+      <div className="footer"></div>
     </div>
   );
 };
